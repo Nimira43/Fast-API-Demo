a663cb50a843c8c2f561b67fffd61de1cbb8caba
Nimira43 <n.rabone72@aol.com>
2025-02-22 22:12:13 +0000

    Counting number of words, displaying first 50, removed  ( and ) from the book text.

--- Entropy-of Written-English/main.py
+++ Entropy-of Written-English/main.py
@@ -1,14 +1,9 @@
-# import requests
-# book1 = requests.get('https://www.gutenberg.org/cache/epub/35/pg35.txt')
-# print(book1)
-
 import re
 
 with open('./books/the-time-machine.txt', 'r', encoding='utf-8') as file:
   book_1 = file.read()
 
 print('CHARACTERS: ', len(book_1))
-print('FIRST 100 CHARACTERS: ', book_1[:100])
 
 strings_to_replace = [
   '\r\n\r\nâ\x80\x9c', 
@@ -25,11 +20,11 @@ for strings_to_match in strings_to_replace :
   regular_expression = re.compile(r'%s' %strings_to_match)
   book_1 = regular_expression.sub(' ', book_1)
 
-print('--------------------------------------')
-print('REMOVED FORMATTING FROM TEXT (FIRST 100): ', book_1[:100])
-
 book_1 = re.sub(r'\s+', ' ', book_1)
 book_1 = book_1.strip()
 
-print('--------------------------------------')
-print('REMOVED WHITESPACE FROM TEXT (FIRST 100): ', book_1[:100])
+print('CLEANED UP TEXT (FIRST 100 CHARACTERS): ', book_1[:100])
+
+words = book_1.split()
+print('NUMBER OF WORDS: ', len(words))
+print('FIRST 50 WORDS:', words[:50])
