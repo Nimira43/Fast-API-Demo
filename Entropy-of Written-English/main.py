076583e0ddb2cd87f2d5a44ad4101ecfafe125fb
Nimira43 <n.rabone72@aol.com>
2025-02-22 13:37:52 +0000

    Bringing in The Time Machine book

--- Entropy-of Written-English/main.py
+++ Entropy-of Written-English/main.py
@@ -1,1 +1,8 @@
-print('Entropy of Written English.')
+# import requests
+# book1 = requests.get('https://www.gutenberg.org/cache/epub/35/pg35.txt')
+# print(book1)
+
+with open('./books/the-time-machine.txt', 'r', encoding='utf-8') as file:
+    book1 = file.read()
+
+print(book1)
