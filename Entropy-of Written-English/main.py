279227edc940ea40cd08790dbda9103ebf48151d
Nimira43 <n.rabone72@aol.com>
2025-02-22 13:53:13 +0000

    Created a variable that has a list of strings that will need to be replaced by a space - ' '

--- Entropy-of Written-English/main.py
+++ Entropy-of Written-English/main.py
@@ -7,3 +7,14 @@ with open('./books/the-time-machine.txt', 'r', encoding='utf-8') as file:
 
 print('CHARACTERS: ', len(book1))
 print('FIRST 2000 CHARACTERS', book1[:2000])
+
+strings2replace = [
+  '\r\n\r\nâ\x80\x9c', 
+  'â\x80\x9c',         
+  'â\x80\x9d',         
+  '\r\n',              
+  'â\x80\x94',         
+  'â\x80\x99',         
+  'â\x80\x98',         
+  '_',                 
+]
